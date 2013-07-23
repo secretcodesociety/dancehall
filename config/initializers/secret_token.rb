e87cda0d8326b86f73c7c5469f43b97acd4b4556
likealiiime <sherrod@softilluminations.com>
2013-07-22 21:22:38 -0400

    Install rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dancehall::Application.config.secret_key_base = '1547bb0c19f7c52e86f75ac829bcef8737c3f00a6c380291d68e863ab182b9933b27d4f3690fc6231ff0760c98aa55867156f28da484d3ddd5ff303760090e8b'
